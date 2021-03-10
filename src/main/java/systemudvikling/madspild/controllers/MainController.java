cc7cdcdca046207087ff6fd92b14aaf2df2795ed
Laust <laust.bonnesen@mail.com>
2021-03-10 18:09:04 +0100

    Lidt på submit fra 3/10/2021

--- src/main/java/systemudvikling/madspild/controllers/MainController.java
+++ src/main/java/systemudvikling/madspild/controllers/MainController.java
@@ -1,10 +1,29 @@
 package systemudvikling.madspild.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import systemudvikling.madspild.model.Event;
 
 @Controller
 public class MainController {
 
     Event event;
+
+    @GetMapping("/makeevent.html")
+    public String createNadver() {
+        return "makeevent.html";
+    }
+
+    @PostMapping("/submit-event")
+    public String createEvent() {
+        return "redirect:/success";
+    }
+
+    @GetMapping("/index.html")
+    public String goBack() {
+        return "index.html";
+    }
+
+
 }
