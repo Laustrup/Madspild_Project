d17b7ccad18f51a1cd2131559edf687ffe1672e3
Laust <laust.bonnesen@mail.com>
2021-03-11 18:25:23 +0100

    Display of makeevent and a little session logic 3/11/2021

--- src/main/java/systemudvikling/madspild/controllers/MainController.java
+++ src/main/java/systemudvikling/madspild/controllers/MainController.java
@@ -1,23 +1,102 @@
 package systemudvikling.madspild.controllers;
 
+import org.apache.catalina.connector.Request;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import systemudvikling.madspild.model.Event;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
 
 @Controller
 public class MainController {
 
-    Event event;
+    private Event event;
+    private ArrayList<Event> events = new ArrayList<>();
+    private HttpSession session;
 
     @GetMapping("/makeevent.html")
     public String createNadver() {
         return "makeevent.html";
     }
+    @GetMapping("/makeeventagain.html")
+    public String nadverError() {
+        return "makeeventagain.html";
+    }
+    @GetMapping("/success.html")
+    public String itsASuccess() {
+        return "success.html";
+    }
 
     @PostMapping("/submit-event")
-    public String createEvent() {
-        return "redirect:/success";
+    public String createEvent(HttpServletRequest request, Model model,
+                              @RequestParam(name = "Type") String type,
+                              @RequestParam(name = "Adress") String adress,
+                              @RequestParam(name = "Description", required = false) String description,
+                              @RequestParam(name = "Hours") String hours,
+                              @RequestParam(name = "Minutes") String minutes,
+                              @RequestParam(name = "GiveAway", required = false) boolean isGiveAway,
+                              @RequestParam(name = "Password") String passWord) {
+
+        boolean returnToMakeevent = checkInputs(type, adress, hours, minutes, passWord, isGiveAway);
+
+        if (returnToMakeevent) {
+            System.out.println("Required inputs was not written...");
+            return "redirect:/makeeventagain.html";
+        }
+        if (isGiveAway != true) {
+            isGiveAway = Boolean.FALSE.equals(isGiveAway);
+        }
+
+        event = new Event(type,adress,description,hours,minutes,isGiveAway,passWord);
+        events.add(event);
+
+        session = request.getSession();
+
+        session.setAttribute("Event", event);
+        System.out.println(session.getAttribute("Event"));
+
+        return "redirect:/success.html";
+    }
+
+    private boolean checkInputs(String type, String adress, String hours, String minutes, String passWord, boolean isGiveAway) {
+        if (type.equals("")) {
+            return true;
+        }
+        else if (adress.equals("")) {
+            return true;
+        }
+        else if (hours.equals("")) {
+            return true;
+        }
+        else if (minutes.equals("")) {
+            return true;
+        }
+        else if (passWord.equals("")) {
+            return true;
+        }
+
+        return false;
+    }
+
+    @GetMapping("/editevent.html")
+    public String deleteEvent(Model model, @RequestParam(name = "Password") String passWord) {
+
+        for (int i = 0; i < events.size(); i++) {
+            if (events.get(i).getPassword().equals(passWord)) {
+                events.remove(events.get(i));
+            }
+        }
+        model = null;
+        model.addAttribute("events", session.getAttribute("Event"));
+
+        System.out.println(session.getAttribute("Event"));
+
+        return "index.html";
     }
 
     @GetMapping("/index.html")
--- src/main/java/systemudvikling/madspild/model/Event.java
+++ src/main/java/systemudvikling/madspild/model/Event.java
@@ -8,13 +8,31 @@ public class Event {
     private String start;
     private String end;
     private boolean isGiveAway;
+    private String password;
 
-    public Event(String type, String address, String description, String start, String end, boolean isGiveAway) {
+    public Event(String type, String address, String description, String start, String end, boolean isGiveAway, String password) {
         this.type = type;
         this.address = address;
         this.description = description;
         this.start = start;
         this.end = end;
         this.isGiveAway = isGiveAway;
+        this.password = password;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String toString() {
+        String giveAway = "";
+
+        if (isGiveAway) {
+            giveAway = " gives væk ";
+        }
+
+        return "type= " + type + " location= " + address + " beskrivelse= " + description +
+                " starttidspunkt= " + start + " sluttidspunkt= " + end + giveAway + "adgangskode er " + password;
     }
 }
