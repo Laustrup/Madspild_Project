9a7a9ca39d2780dc27b7bdeaee51f36d6cd91c06
Laust <laust.bonnesen@mail.com>
2021-03-13 17:10:02 +0100

    Different kinds of exceptions

--- src/main/java/systemudvikling/madspild/controllers/MainController.java
+++ src/main/java/systemudvikling/madspild/controllers/MainController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import systemudvikling.madspild.model.Event;
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.util.ArrayList;
 import java.util.InputMismatchException;
@@ -17,10 +16,29 @@ public class MainController {
 
     private Event event;
     private ArrayList<Event> events = new ArrayList<>();
-    private HttpSession session;
+    private String typeOfRedirect = new String();
+
+    private boolean isAllowedInput;
+    private boolean returnToMakeevent;
+    private boolean isPasswordTaken;
 
     @GetMapping("/makeevent.html")
-    public String createNadver() {
+    public String createNadver(Model model) {
+
+        if (returnToMakeevent && typeOfRedirect.equals("Not written")) {
+            model.addAttribute("Exception", "Alle felter med * skal besvares!");
+            System.out.println("Reached not written statement");
+        }
+        else if (returnToMakeevent && typeOfRedirect.equals("Time exception")) {
+            model.addAttribute("Exception", "Tidsformattet er forkert!");
+            System.out.println("Reached time exception statement");
+        }
+        else if (returnToMakeevent && isPasswordTaken) {
+            model.addAttribute("Exception", "Adgangskode findes allerede...");
+            System.out.println("Reached password is taken statement");
+        }
+        returnToMakeevent = false;
+
         return "makeevent.html";
     }
     @GetMapping("/makeeventagain.html")
@@ -63,37 +81,25 @@ public class MainController {
     }
 
     @PostMapping("/submit-event")
-    public String createEvent(HttpServletRequest request,
-                              @RequestParam(name = "Type") String type,
-                              @RequestParam(name = "Adress") String adress,
-                              @RequestParam(name = "Description", required = false) String description,
-                              @RequestParam(name = "Hours") String hours,
-                              @RequestParam(name = "Minutes") String minutes,
-                              @RequestParam(name = "GiveAway", required = false) boolean isGiveAway,
-                              @RequestParam(name = "Password") String passWord, RedirectAttributes attributes) {
+    public String createEvent(Model model,
+            @RequestParam(name = "Type") String type,
+            @RequestParam(name = "Adress") String adress,
+            @RequestParam(name = "Description", required = false) String description,
+            @RequestParam(name = "Hours") String hours,
+            @RequestParam(name = "Minutes") String minutes,
+            @RequestParam(name = "GiveAway", required = false) boolean isGiveAway,
+            @RequestParam(name = "Password") String passWord, RedirectAttributes attributes) {
 
-        boolean isAllowedInput = checkInputs(type, adress, hours, minutes, passWord);
+        boolean hasException = checkForEvent(type,adress,description,hours,minutes,passWord,isGiveAway);
 
-        boolean returnToMakeevent = CheckIfReturn(isAllowedInput, isGiveAway,
-                type,adress,description,hours,minutes,passWord);
-
-        if (returnToMakeevent) {
-            return "redirect:/makeeventagain.html";
-        }
-        else {
-            event = new Event(type,adress,description,hours,minutes,isGiveAway,passWord);
+        if (hasException) {
+            return "redirect:/makeevent.html";
         }
 
+        event = new Event(type,adress,description,hours,minutes,isGiveAway,passWord);
         events.add(event);
 
-        session = request.getSession();
-
-        /*
-        session.setAttribute("Event", event);
-
-        System.out.println(session.getAttribute("Event"));
-         */
-
+        model.addAttribute("Exception", "");
         attributes.addAttribute("Type", type);
         attributes.addAttribute("Adress", adress);
         attributes.addAttribute("Description", description);
@@ -102,33 +108,35 @@ public class MainController {
         attributes.addAttribute("GiveAway", isGiveAway);
         attributes.addAttribute("Password", passWord);
 
+
         return "redirect:/success.html";
     }
 
     private boolean checkInputs(String type, String adress, String hours, String minutes, String passWord) {
         if (type.equals("")) {
-            return true;
+            return false;
         }
         else if (adress.equals("")) {
-            return true;
+            return false;
         }
         else if (hours.equals("")) {
-            return true;
+            return false;
         }
         else if (minutes.equals("")) {
-            return true;
+            return false;
         }
         else if (passWord.equals("")) {
-            return true;
+            return false;
         }
 
-        return false;
+        return true;
     }
 
-    private boolean CheckIfReturn(boolean isAllowedInput, boolean isGiveAway, String type, String adress,
+    private boolean checkIfReturn(boolean isAllowedInput, boolean isGiveAway, String type, String adress,
                                   String description, String hours, String minutes, String passWord) {
-        if (isAllowedInput) {
+        if (!isAllowedInput) {
             System.out.println("Required inputs was not written...");
+            typeOfRedirect = "Not written";
             return true;
         }
         if (isGiveAway != true) {
@@ -139,12 +147,38 @@ public class MainController {
             event = new Event(type,adress,description,hours,minutes,isGiveAway,passWord);
         }
         catch (IllegalArgumentException e) {
+            typeOfRedirect = "Time exception";
             return true;
         }
         catch (InputMismatchException e) {
+            typeOfRedirect = "Time exception";
             return true;
         }
 
+        return false;
+    }
+
+    private boolean checkPassword(String password) {
+        for (int i = 0; i < events.size(); i++) {
+            if (events.get(i).getPassword() == password) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean checkForEvent(String type, String adress, String description, String hours, String minutes,
+                                   String passWord, boolean isGiveAway) {
+        isAllowedInput = checkInputs(type, adress, hours, minutes, passWord);
+
+        returnToMakeevent = checkIfReturn(isAllowedInput, isGiveAway,
+                type,adress,description,hours,minutes,passWord);
+
+        isPasswordTaken = checkPassword(passWord);
+
+        if (returnToMakeevent) {
+            return true;
+        }
         return false;
     }
 
